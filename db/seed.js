2bc756b9a76f41e50fc640752b4548515266d0ff
gedunlap <g.e.dunlap90@gmail.com>
2021-03-20 11:58:15 -0400

    added seed

---
+++ db/seed.js
@@ -0,0 +1,21 @@
+// improt database connection
+const mongoose = require('./connection')
+
+//////////////////////////////////////
+// IMPORT YOUR MODELS BELOW
+////////////////////////////////////
+
+
+
+///////////////////////////////////////////////////
+// DO YOUR DATABASE OPERATIONS IN BELOW FUNCTION
+/////////////////////////////////////////////////
+
+const seed = async () => {
+    /*--------- Code Goes Here-----/
+    /----------------------------/
+    console.log("seed file")--*/
+}
+
+// Run Seed Function
+seed()
